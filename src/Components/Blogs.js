65974dcf9f3a93b37b4a75939299ca325551a96d
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-05-30 12:20:47 +0600

    news-app

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -7,7 +7,7 @@ import "../styling/blogs.css";
 
 const Blogs = () => {
   const searchInput = useSelector(selectUserInput);
-  const blog_url = `https://newsapi.org/v2/top-headlines?country=${searchInput}us&apiKey=810c3dce266a492c8337ae5ea8e21168`;
+  const blog_url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=810c3dce266a492c8337ae5ea8e21168`;
   const dispatch = useDispatch();
   const [blogs, setBlogs] = useState();
 
@@ -47,7 +47,7 @@ const Blogs = () => {
 
         {blogs?.totalArticles === 0 && (
           <h1 className="no__blogs">
-            No blogs available. Search something else to read blogs on the
+            No blogs available 😞. Search something else to read blogs on the
             greatest platform.
           </h1>
         )}
